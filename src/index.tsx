72510abfbbb5f8306de1ce30df9698c79ff8cf9f
Alex Muench <ammuench@gmail.com>
2018-11-01 20:26:40 -0500

    Resolve issues with css loader, upgrade inferno to v6

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,17 @@
-import { render, version, Component } from 'inferno';
+import { Component, render, version  } from 'inferno';
 import { Incrementer } from './components/Incrementer';
+
+import './index.css';
 
 const container = document.getElementById('app');
 
 class MyComponent extends Component<any, any> {
-	private tsxVersion: number;
+	private tsxVersion: string;
 
 	constructor(props, context) {
 		super(props, context);
 
-		this.tsxVersion = 3.0.3; /* This is typed value */
+		this.tsxVersion = '3.0.3'; /* This is typed value */
 	}
 
 	public render() {
